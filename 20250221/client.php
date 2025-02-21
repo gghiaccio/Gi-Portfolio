1e864310b1d6ae532481b600edc851b7dc2b8763
Unknown User <giaada.donizetti@gmail.com>
2025-02-21 18:32:45 +0100

    Create client.php

---
+++ 20250221/client.php
@@ -0,0 +1,38 @@
+<?php
+// URL del file WSDL
+$wsdl_url = "http://URL_AL_DEL_FILE_WSDL";
+
+// check se il modulo di input ha inviato un nome
+if (isset($_POST['name'])) 
+{
+    // check che non sia nullo
+    if ($_POST['name'] != null) 
+    {
+        // creo client SOAP utilizzando il WSDL
+        $client = new SoapClient($wsdl_url);
+
+        // richiamo il salutino
+        $response = $client->Salutino(htmlentities($_POST['name']));
+
+        // stampo la response
+        print_r($response);
+    }
+}
+?>
+  
+<style>
+    body 
+    {
+        background-color: gray;
+        color: white;
+        font-family: "Times New Roman", Times, serif;
+    }
+    h1 
+    {
+        text-align: center;
+    }
+    p, label, input, button 
+    {
+        color: white;
+    }
+</style>
